b80b15093ec042b3380a3e4a3285f60b5b57836c
Christian Perez <perezchristian1012@gmail.com>
2019-09-10 13:21:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,14 +3,14 @@ require_relative "./user.rb"
 class Student < User
 
   def initialize
-    @knowledge = []
+    @@knowledge = []
   end
 
   def learn(string)
-    @knowledge << "Ruby framework Rails gem bundle update"
+    @@knowledge << "Ruby framework Rails gem bundle update"
   end
 
   def knowledge
-    @knowledge
+    @@knowledge
   end
 end
